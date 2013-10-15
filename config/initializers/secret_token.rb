157583f4f1f70a1ef3f0f98d3da758b8775a9870
Harrisam <samantha.s.harris@googlemail.com>
2013-10-15 12:16:36 +0100

    set up of websockets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Websockseg::Application.config.secret_key_base = 'f477f199ad12ab6ad923d0ecfdb93597b7e73d9ca597b12300602a6458d7565bbd242147dbe0419c5f1b91aa4658fbfbf0154b612796ed7fb2d557942108a7df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Websockseg::Application.config.session_store :cookie_store, key: '_websockseg_session'
